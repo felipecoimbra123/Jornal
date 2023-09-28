025e5ca44abd6c385eddafc49da0dbf0e456f9d5
felipecoimbra123 <137563995+felipecoimbra123@users.noreply.github.com>
2023-09-28 16:57:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// Faz o menu hamburguinho
+const menu = document.querySelector(".menu")
+const links = document.querySelector(".nav_links ul")
+menu.addEventListener("click", abrirMenu)
+
+function abrirMenu() {
+    // Quando clica faz isso
+    if (links.style.display == "flex") {
+        links.style.display = "none"
+    } else {
+        links.style.display = "flex"
+    }
+}
+
+window.onresize = function () {
+    // Quando a janela é redimensionada muda o estilo
+    if (window.innerWidth >= 850) {
+        links.style.display = "flex"
+    }
+    else {
+        links.style.display = "none"
+    }
+}
+
+// Adiciona uma legenda ao passar o mouse por cima
+const legendas = document.querySelectorAll(".body-main figcaption")
+for (let pos in legendas) {
+    legendas[pos].title = legendas[pos].innerText
+}
+
+// Faz com que seja possível pesquisar
+const inpPesquisa = document.querySelector("#pesquisa")
+let txtLegendas = []
+inpPesquisa.oninput = function (e) {
+    if (e.target.value != "") {
+        valorPesquisa = e.target.value.toLowerCase()
+        for (let pos in legendas) {
+            text = legendas[pos].textContent
+            text = String(text)
+            text = text.toLowerCase()
+            txtLegendas.push(text)
+            if (txtLegendas[pos].includes(valorPesquisa)) {
+                legendas[pos].style.color = "white"
+                legendas[pos].style.background = "var(--cor-principal)"
+            } else {
+                legendas[pos].style.background = "rgba(0, 0, 0, 0.637)"
+                legendas[pos].style.color = "white"
+            }
+        }
+    } else {
+        for (let pos in legendas) {
+            legendas[pos].style.background = "rgba(0, 0, 0, 0.637)"
+            legendas[pos].style.color = "white"
+        }
+    }
+}
